85f2342f185ad421e65bad5990d3e8bd6a94feaf
Goxiaoy <xy1996mail@gmail.com>
2021-08-29 21:08:11 +0800

    added json

---
+++ pkg/data/json.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+type JSON json.RawMessage
+
+// Scan value into Jsonb, implements sql.Scanner interface
+func (j *JSON) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+
+	result := json.RawMessage{}
+	err := json.Unmarshal(bytes, &result)
+	*j = JSON(result)
+	return err
+}
+
+// Value return json value, implement driver.Valuer interface
+func (j JSON) Value() (driver.Value, error) {
+	if len(j) == 0 {
+		return nil, nil
+	}
+	return json.RawMessage(j).MarshalJSON()
+}
--- pkg/data/repo.go
+++ pkg/data/repo.go
@@ -1,16 +1,11 @@
 package data
 
-import (
-	"context"
-	"github.com/a8m/rql"
-)
-
-type Repo[T interface{}] interface {
-	List(ctx context.Context, query interface{}) ([]*T, error)
-	First(ctx context.Context, query interface{})(*T, error)
-	Count(ctx context.Context, query interface{}) (total int64, filtered int64, err error)
-	Get(ctx context.Context, id string) (*T, error)
-	Create(ctx context.Context, group *T) error
-	Update(ctx context.Context, id string, group *T, p rql.Select) error
-	Delete(ctx context.Context, id string) error
-}
+//type Repo[T interface{}] interface {
+//	List(ctx context.Context, query interface{}) ([]*T, error)
+//	First(ctx context.Context, query interface{})(*T, error)
+//	Count(ctx context.Context, query interface{}) (total int64, filtered int64, err error)
+//	Get(ctx context.Context, id string) (*T, error)
+//	Create(ctx context.Context, group *T) error
+//	Update(ctx context.Context, id string, group *T, p rql.Select) error
+//	Delete(ctx context.Context, id string) error
+//}
