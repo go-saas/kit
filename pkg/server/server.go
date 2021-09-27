9f5ff47da37fbb909774f1da5cff510ca9e7cf76
Goxiaoy <xy1996mail@gmail.com>
2021-09-27 16:32:29 +0800

    config allow headers

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -46,7 +46,7 @@ func PatchHttpOpts(opts []http.ServerOption, name string, services *conf.Service
 		opts = append(opts, http.Filter(handlers.CORS(
 			handlers.AllowedOrigins(server.Http.Cors.GetAllowedOrigins()),
 			handlers.AllowedMethods(server.Http.Cors.GetAllowedMethods()),
-			handlers.AllowedHeaders([]string{"Content-Type","Authorization"}),
+			handlers.AllowedHeaders(append([]string{"Content-Type", "Authorization"}, server.Http.Cors.AllowedHeaders...)),
 		)))
 	}
 	return opts
