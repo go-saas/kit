437152e574b1572b023936ba9312dfa6454288b2
Goxiaoy <xy1996mail@gmail.com>
2022-05-26 23:05:47 +0800

    fix: format error

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/v2/transport/http"
@@ -50,7 +51,7 @@ func NewGrpcConn(
 	fOpts := []grpc.ClientOption{
 		grpc.WithEndpoint(endpoint.GrpcEndpoint),
 		grpc.WithMiddleware(
-			server.Recovery(),
+			recovery.Recovery(),
 			tracing.Client(),
 			ClientPropagation(clientCfg, opt, tokenMgr, logger),
 		),
@@ -101,7 +102,7 @@ func NewHttpClient(
 	fOpts := []http.ClientOption{
 		http.WithEndpoint(endpoint.HttpEndpoint),
 		http.WithMiddleware(
-			server.Recovery(),
+			recovery.Recovery(),
 			tracing.Client(),
 			ClientPropagation(clientCfg, opt, tokenMgr, logger),
 		),
