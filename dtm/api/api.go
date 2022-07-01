94ddc7457185bc53d5bbfa4725a43d3b7f9bd6a6
Goxiaoy <xy1996mail@gmail.com>
2022-07-01 13:53:13 +0800

    feat: dtm UnaryInterceptor

--- dtm/api/api.go
+++ dtm/api/api.go
@@ -1,3 +1,14 @@
 package api
 
-//reuse middleware
+import (
+	"github.com/dtm-labs/dtmgrpc"
+	klog "github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-saas/kit/pkg/api"
+	"github.com/go-saas/kit/pkg/conf"
+)
+
+func NewInit(client *conf.Client, opt *api.Option, tokenMgr api.TokenManager, logger klog.Logger) {
+	m := api.ClientPropagation(client, opt, tokenMgr, logger)
+	dtmgrpc.AddUnaryInterceptor(api.UnaryClientInterceptor([]middleware.Middleware{m}, 0, nil))
+}
