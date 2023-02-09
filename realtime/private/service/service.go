6aaa8c8cec674595c484836414afb2d53d08f848
Goxiaoy <xy1996mail@gmail.com>
2023-02-09 14:23:45 +0800

    fix: openapi

--- realtime/private/service/service.go
+++ realtime/private/service/service.go
@@ -37,7 +37,7 @@ func NewHttpServerRegister(
 		swaggerRouter := chi.NewRouter()
 		swaggerRouter.Use(
 			kithttp.MiddlewareConvert(errEncoder, middleware...))
-		const apiPrefix = "/v1/server/dev/swagger"
+		const apiPrefix = "/v1/realtime/dev/swagger"
 		swaggerRouter.Handle(apiPrefix+"*", http.StripPrefix(apiPrefix, swaggerui.Handler(spec)))
 	})
 }
