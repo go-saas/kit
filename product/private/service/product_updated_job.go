274c7a2452a16616fc7e47f08f37de9e41f06fb9
Goxiaoy <xy1996mail@gmail.com>
2023-10-27 16:33:32 +0800

    update

--- product/private/service/product_updated_job.go
+++ product/private/service/product_updated_job.go
@@ -14,6 +14,7 @@ import (
 	stripeclient "github.com/stripe/stripe-go/v76/client"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/protobuf/encoding/protojson"
+	"strings"
 	"time"
 )
 
@@ -68,7 +69,7 @@ func syncWithStripe(ctx context.Context, client *stripeclient.API, repo biz.Prod
 	stripeInfo, find := lo.Find(links, func(link biz.ProductSyncLink) bool {
 		return string(biz.ProductManageProviderStripe) == link.ProviderName
 	})
-	var stripeId string
+	var stripeProductId string
 	if !find {
 		// create stripe object
 		params := mapBizProduct2Stripe(product)
@@ -76,32 +77,75 @@ func syncWithStripe(ctx context.Context, client *stripeclient.API, repo biz.Prod
 		if err != nil {
 			return err
 		}
-		stripeId = stripeProduct.ID
+		stripeProductId = stripeProduct.ID
 		t := time.Now()
 		err = repo.UpdateSyncLink(ctx, product, &biz.ProductSyncLink{
 			ProviderName: string(biz.ProductManageProviderStripe),
-			ProviderId:   stripeId,
+			ProviderId:   stripeProductId,
 			LastSyncTime: &t,
 		})
 		if err != nil {
 			return err
 		}
 	} else {
-		stripeId = stripeInfo.ProviderId
+		stripeProductId = stripeInfo.ProviderId
 		//update product if needed
-		stripeProduct, err := client.Products.Get(stripeId, &stripe.ProductParams{})
+		stripeProduct, err := client.Products.Get(stripeProductId, &stripe.ProductParams{})
 		if stripeProduct.Metadata["version"] != version {
 			klog.Infof("product_id:%s version:%s same with stipe, skip updates", product.ID.String(), version)
 			return nil
 		}
 		params := mapBizProduct2Stripe(product)
-		_, err = client.Products.Update(stripeId, params)
+		_, err = client.Products.Update(stripeProductId, params)
 		if err != nil {
 			return err
 		}
 	}
 
-	//TODO update price
+	// update price
+	allKeys := lo.Map(product.Prices, func(t biz.Price, _ int) string {
+		return t.ID.String()
+	})
+	priceIter := client.Prices.List(&stripe.PriceListParams{Product: &stripeProductId})
+	if priceIter.Err() != nil {
+		return priceIter.Err()
+	}
+	var stripePrices []*stripe.Price
+	for priceIter.Next() {
+		if priceIter.Err() != nil {
+			return priceIter.Err()
+		}
+		stripePrices = append(stripePrices, priceIter.Price())
+	}
+	//delete  prices
+	toDeactivate := lo.Filter(stripePrices, func(price *stripe.Price, _ int) bool {
+		return !lo.Contains(allKeys, price.LookupKey)
+	})
+	for _, price := range toDeactivate {
+		active := false
+		_, err = client.Prices.Update(price.ID, &stripe.PriceParams{Active: &active})
+		if err != nil {
+			return err
+		}
+	}
+	for _, price := range product.Prices {
+		stripePrice, ok := lo.Find(stripePrices, func(p *stripe.Price) bool {
+			return p.LookupKey == price.ID.String()
+		})
+		params := mapBizPrice2Stripe(stripeProductId, &price)
+		if !ok {
+			//create
+			_, err = client.Prices.New(params)
+			if err != nil {
+				return err
+			}
+		} else {
+			_, err = client.Prices.Update(stripePrice.ID, params)
+			if err != nil {
+				return err
+			}
+		}
+	}
 
 	return nil
 }
@@ -114,6 +158,51 @@ func mapBizProduct2Stripe(product *biz.Product) *stripe.ProductParams {
 		Shippable:   stripe.Bool(product.NeedShipping),
 		Metadata:    map[string]string{"version": product.Version.String},
 		//TODO type
-
 	}
+}
+
+func mapBizPrice2Stripe(stripeProductId string, price *biz.Price) *stripe.PriceParams {
+	r := &stripe.PriceParams{
+		BillingScheme: stripe2.String(string(price.BillingScheme)),
+		Currency:      stripe2.String(strings.ToLower(price.CurrencyCode)),
+		LookupKey:     stripe2.String(price.ID.String()),
+		Product:       stripe2.String(stripeProductId),
+		Tiers:         nil,
+		TiersMode:     stripe2.String(string(price.TiersMode)),
+		TransformQuantity: &stripe.PriceTransformQuantityParams{
+			DivideBy: stripe.Int64(price.TransformQuantity.DivideBy),
+			Round:    stripe2.String(string(price.TransformQuantity.Round)),
+		},
+		UnitAmount: stripe.Int64(price.DefaultAmount),
+	}
+	r.CurrencyOptions = lo.SliceToMap(price.CurrencyOptions, func(t biz.PriceCurrencyOption) (string, *stripe.PriceCurrencyOptionsParams) {
+		return strings.ToLower(t.CurrencyCode), &stripe.PriceCurrencyOptionsParams{
+			Tiers: lo.Map(t.Tiers, func(t biz.PriceCurrencyOptionTier, _ int) *stripe.PriceCurrencyOptionsTierParams {
+				return &stripe.PriceCurrencyOptionsTierParams{
+					FlatAmount: stripe.Int64(t.FlatAmount),
+					UnitAmount: stripe.Int64(t.UnitAmount),
+					UpTo:       stripe.Int64(t.UpTo),
+				}
+			}),
+			UnitAmount: stripe.Int64(t.DefaultAmount),
+		}
+	})
+	if price.Recurring != nil {
+		r.Recurring = &stripe.PriceRecurringParams{
+			AggregateUsage:  stripe2.String(string(price.Recurring.AggregateUsage)),
+			Interval:        stripe2.String(string(price.Recurring.Interval)),
+			IntervalCount:   stripe.Int64(price.Recurring.IntervalCount),
+			TrialPeriodDays: stripe.Int64(price.Recurring.TrialPeriodDays),
+			UsageType:       stripe2.String(string(price.Recurring.UsageType)),
+		}
+	}
+	r.Tiers = lo.Map(price.Tiers, func(t biz.PriceTier, _ int) *stripe.PriceTierParams {
+		return &stripe.PriceTierParams{
+			FlatAmount: stripe.Int64(t.FlatAmount),
+			UnitAmount: stripe.Int64(t.UnitAmount),
+			UpTo:       stripe.Int64(t.UpTo),
+		}
+	})
+
+	return r
 }
